793876ff1805ac22e226578fcc78bce792061b9f
yacine-benichou <yacine.benichou@gmail.com>
2021-08-29 23:40:36 +0200

    No responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Drumpad from './Drumpad';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      power: true,
+      clipId: "",
+      keyTrigger: 'Q',
+      url: "",
+      display: ""
+    }
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.updateDisplay = this.updateDisplay.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress)
+}
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  updateDisplay(keyValue) {
+    this.setState({
+      display: keyValue
+    })
+  }
+
+  handleKeyPress(e) {
+    const filteredList = list.find(element => e.keyCode === element.keyCode);
+    try {
+      if(filteredList.keyTrigger) {
+        this.setState({
+            keyTrigger: filteredList.keyTrigger,
+            url: filteredList.url,
+            clipId: filteredList.id,
+            display: filteredList.id
+        })
+        let sound = document.getElementById(this.state.keyTrigger);
+        const playPromise = sound.play();
+
+        if(playPromise !== undefined) {
+          playPromise.then().catch(error => console.warn("error prevented"))
+        }
+      }
+    } catch(exception) {
+      console.warn("No corresponding key found.");
+    }
+  }
+
+  render() {
+
+    return(
+      <div id = "display" className = "display-container">{this.state.display}
+        <div id = "drum-machine" className = "outer-container">
+          <Drumpad display = {this.state.display} handleClick = {this.handleClick} handlekeyPress = {this.handleKeyPress} List = {list} keyTrigger = {this.state.keyTrigger} url = {this.state.url} clipId = {this.state.clipId} updateDisplay = {this.updateDisplay} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Drumpad.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+class Drumpad extends React.Component {
+    constructor(props) {
+        super(props);
+        this.audio = React.createRef();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        const sound = event.target.children[0];
+        const parentId = event.target.id;
+        sound.currentTime = 0;
+        const playPromise = sound.play();
+
+        if(playPromise !== undefined) {
+          playPromise.then().catch(error => console.warn("error prevented"))
+        }
+        this.props.updateDisplay(parentId);
+    }
+
+
+    render() {
+        return(
+            <div id = "drumpad" className = "mid-container">
+                <div className = "inner-container">
+                    <div className = "drum-pads">
+                        { /* this.props.List.map((element) => (
+                            <button className = "drum-pad" id = {this.props.clipId === element.id} onClick = {this.handleClick}  onKeyPress = {this.props.handleKeyPress} >
+                                <audio className = "clip" id = {this.props.keyTrigger === element.keyTrigger} src = {this.props.url === element.url} ref = {this.audio} />{this.props.keyTrigger}
+                            </button>
+                        )) */ }
+                        <button className = "drum-pad" id = "Heater-1" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio id = "Q" className = "clip" src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" type = "audio/mp3" />
+                        Q
+                        </button>
+                        <button className="drum-pad" id="Heater-2" onClick={this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" />
+                        W
+                        </button>
+                        <button className="drum-pad" id="Heater-3" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "E" src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" />
+                        E
+                        </button>
+                        <button className="drum-pad" id="Heater-4" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "A" src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" />
+                        A
+                        </button>
+                        <button className="drum-pad" id="Clap" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "S" src = "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" />
+                        S
+                        </button>
+                        <button className="drum-pad" id="Open-HH" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "D" src = "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" />
+                        D
+                        </button>
+                        <button className="drum-pad" id="Kick-n'Hat" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "Z" src = "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" />
+                        Z
+                        </button>
+                        <button className="drum-pad" id="Kick" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "X" src = "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" />
+                        X
+                        </button>
+                        <button className="drum-pad" id="Closed-HH" onClick = {this.handleClick} onKeyPress = {this.props.handleKeyPress}>
+                            <audio className = "clip" id = "C" src = "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" />
+                        C
+                    </button> 
+                </div>
+            </div> 
+        </div>
+        )
+    }
+}
+
+export default Drumpad;
